b1506eeb061943af5907fe0f3cf15fce43541283
Akshithaaashi <akshitha.aashi@gmail.com>
2021-09-03 22:35:14 +0530

    Added : emp part time

--- src/com/empwage/EmployeeWage.java
+++ src/com/empwage/EmployeeWage.java
@@ -1,24 +1,38 @@
 package com.empwage;
 
 public class EmployeeWage{
-	public static void main(String[] args){
-        System.out.println("Welcome to Employee Wage Computation");
-        int isFullTime = 1;
-        int empRatePerHour = 20;
 
-        int empHours = 0;
-        int empWage  = 0;
+	public static void main(String args[])
 
+	{
+	 System.out.println("Welcome to empwage computation");
+	
+		int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
 
-        double empCheck = Math.floor(Math.random()*10) % 2;
-        if(empCheck == isFullTime)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
-        empHours = 8;
+        int empHrs = 0;
+        int empwage = 0;
 
-        empWage = empHours * empRatePerHour;
-        System.out.println("Emp Wage: " + empWage);
+        double empcheck = Math.floor(Math.random() * 10) % 3; // generate random no 0 or 1 or 2 
 
-    }
+        if (empcheck == IS_FULL_TIME)//comdition to check full time
+		{
+                empHrs = 8;
+				System.out.println("Employee is present full time");
+	    }
+		else if (empcheck == IS_PART_TIME)//comdition to check part time
+		{
+			
+                empHrs = 4;
+				System.out.println("Employee is present part time");
+	    }
+		else
+		{	
+				System.out.println("Employee is absent");
+                empHrs = 0;
+		}
+				empwage = empHrs * EMP_RATE_PER_HOUR;//calculate empwage 
+				System.out.println("Employee Wage : " + empwage);
+	}
 }
