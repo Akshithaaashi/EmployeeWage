b152d247b78ff2691cc3d41d8175256c80089da0
Akshithaaashi <akshitha.aashi@gmail.com>
2021-09-03 22:40:10 +0530

    Added : calculating wages for a month

--- src/com/empwage/EmployeeWage.java
+++ src/com/empwage/EmployeeWage.java
@@ -1,33 +1,37 @@
 package com.empwage;
 
 public class EmployeeWage{
-
-
-    public static final int isFullTime = 2;
+	public static final int isFullTime = 2;
     public static final int isPartTime = 1;
     public static final int empRatePerHour = 20;
+    public static final int numOfWorkingDays = 20;
 
-public static void main(String[] args){
 
-    int empHours = 0;
-    int empWage  = 0;
+    public static void main(String[] args){
 
-    int empCheck = (int) Math.floor(Math.random()*10) % 3;
-    switch(empCheck){
-        case isFullTime:
-            empHours = 8;
-            break;
-        case isPartTime:
-            empHours = 4;
-            break;
-        default :
-            empHours = 0;
+        int empHours = 0;
+        int empWage  = 0;
+        int totalEmpWage = 0;
+
+        for(int day =0; day < numOfWorkingDays; day++) {
+            int empCheck = (int) Math.floor(Math.random()*10) % 3;
+            switch(empCheck){
+                case isFullTime:
+                    empHours = 8;
+                    break;
+                case isPartTime:
+                    empHours = 4;
+                    break;
+                default :
+                    empHours = 0;
+            }
+
+            empWage = empHours * empRatePerHour;
+            totalEmpWage += empWage;
+        }
+
+
+
+        System.out.println("Emp Wage for 20 working days: " + totalEmpWage);
     }
-
-
-
-    empWage = empHours * empRatePerHour;
-    System.out.println("Emp Wage: " + empWage);
-
-}
 }
