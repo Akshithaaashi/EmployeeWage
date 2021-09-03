ec4699cd8388f8a96e3098a506548fa7bc815195
Akshithaaashi <akshitha.aashi@gmail.com>
2021-09-03 22:38:06 +0530

    Added : solving using switch statement

--- src/com/empwage/EmployeeWage.java
+++ src/com/empwage/EmployeeWage.java
@@ -2,37 +2,32 @@ package com.empwage;
 
 public class EmployeeWage{
 
-	public static void main(String args[])
 
-	{
-	 System.out.println("Welcome to empwage computation");
-	
-		int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+    public static final int isFullTime = 2;
+    public static final int isPartTime = 1;
+    public static final int empRatePerHour = 20;
 
-        int empHrs = 0;
-        int empwage = 0;
+public static void main(String[] args){
 
-        double empcheck = Math.floor(Math.random() * 10) % 3; // generate random no 0 or 1 or 2 
+    int empHours = 0;
+    int empWage  = 0;
 
-        if (empcheck == IS_FULL_TIME)//comdition to check full time
-		{
-                empHrs = 8;
-				System.out.println("Employee is present full time");
-	    }
-		else if (empcheck == IS_PART_TIME)//comdition to check part time
-		{
-			
-                empHrs = 4;
-				System.out.println("Employee is present part time");
-	    }
-		else
-		{	
-				System.out.println("Employee is absent");
-                empHrs = 0;
-		}
-				empwage = empHrs * EMP_RATE_PER_HOUR;//calculate empwage 
-				System.out.println("Employee Wage : " + empwage);
-	}
+    int empCheck = (int) Math.floor(Math.random()*10) % 3;
+    switch(empCheck){
+        case isFullTime:
+            empHours = 8;
+            break;
+        case isPartTime:
+            empHours = 4;
+            break;
+        default :
+            empHours = 0;
+    }
+
+
+
+    empWage = empHours * empRatePerHour;
+    System.out.println("Emp Wage: " + empWage);
+
+}
 }
